68c966ad9677c2de2ac675fcec7814b9cbecffa4
stfx <stfx@hotmail.de>
2012-08-27 03:14:51 +0200

    [s1679] Finalize Outdoor PvP Implementation
    Implementation by Xfurry and stfx
    
    Thanks to all original Outdoor PvP authors - especially Balrok
    Thanks to X-Savior for the proper data
    Thanks to crackm for help with database
    
    And thanks to all people who contributed and/or posted bug reports
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>
    
    (based on commit 9841dcd)
    
    Signed-off-by: Nighoo <nighoo@overninethousand.de>

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -49,6 +49,7 @@
 #include "CreatureAIRegistry.h"
 #include "Policies/SingletonImp.h"
 #include "BattleGroundMgr.h"
+#include "OutdoorPvP/OutdoorPvP.h"
 #include "TemporarySummon.h"
 #include "VMapFactory.h"
 #include "MoveMap.h"
@@ -1310,6 +1311,10 @@ void World::SetInitialWorldSettings()
     sBattleGroundMgr.CreateInitialBattleGrounds();
     sBattleGroundMgr.InitAutomaticArenaPointDistribution();
 
+    ///- Initialize Outdoor PvP
+    sLog.outString("Starting Outdoor PvP System");
+    sOutdoorPvPMgr.InitOutdoorPvP();
+
     // Not sure if this can be moved up in the sequence (with static data loading) as it uses MapManager
     sLog.outString("Loading Transports...");
     sMapMgr.LoadTransports();
@@ -1464,6 +1469,7 @@ void World::Update(uint32 diff)
     ///- Update objects (maps, transport, creatures,...)
     sMapMgr.Update(diff);
     sBattleGroundMgr.Update(diff);
+    sOutdoorPvPMgr.Update(diff);
 
     ///- Delete all characters which have been deleted X days before
     if (m_timers[WUPDATE_DELETECHARS].Passed())
