da77dd9ccf86e72d1242af9959b461060d6b8991
Velvet <vladimir.kontsevich@gmail.com>
2012-10-08 18:25:38 +0300

    [00073] Cherry-Pick...
    - Build the core before building the script bindings.
    
    Signed-off-by: TheLuda <theluda@getmangos.com>
    [s1702] Clean mangos.sql and fix some missing changes.
    
    Signed-off-by: Salja <salja2012@hotmail.de>
    [s1703] Improve pathfinding for swimming mobs, also consider dynamic flying as flying
    - Fixed VMAP extractor build to properly link against libmpq.
    
    Signed-off-by: TheLuda <theluda@getmangos.com>
    [s1709] Fix some problems detected by static code analysis

--- src/game/PathFinder.cpp
+++ src/game/PathFinder.cpp
@@ -180,8 +180,19 @@ void PathFinder::BuildPolyPath(const Vector3& startPos, const Vector3& endPos)
     {
         DEBUG_FILTER_LOG(LOG_FILTER_PATHFINDING, "++ BuildPolyPath :: (startPoly == 0 || endPoly == 0)\n");
         BuildShortcut();
-        m_type = (m_sourceUnit->GetTypeId() == TYPEID_UNIT && ((Creature*)m_sourceUnit)->CanFly())
-                 ? PathType(PATHFIND_NORMAL | PATHFIND_NOT_USING_PATH) : PATHFIND_NOPATH;
+
+        // Check for swimming or flying shortcut
+        if (m_sourceUnit->GetTypeId() == TYPEID_UNIT)
+        {
+            if ((startPoly == INVALID_POLYREF && m_sourceUnit->GetTerrain()->IsUnderWater(startPos.x, startPos.y, startPos.z)) ||
+                    (endPoly == INVALID_POLYREF && m_sourceUnit->GetTerrain()->IsUnderWater(endPos.x, endPos.y, endPos.z)))
+                m_type = ((Creature*)m_sourceUnit)->CanSwim() ? PathType(PATHFIND_NORMAL | PATHFIND_NOT_USING_PATH) : PATHFIND_NOPATH;
+            else
+                m_type = ((Creature*)m_sourceUnit)->CanFly() ? PathType(PATHFIND_NORMAL | PATHFIND_NOT_USING_PATH) : PATHFIND_NOPATH;
+        }
+        else
+            m_type = PATHFIND_NOPATH;
+
         return;
     }
 
@@ -581,7 +592,7 @@ uint32 PathFinder::fixupCorridor(dtPolyRef* path, uint32 npath, uint32 maxPath,
     // Adjust beginning of the buffer to include the visited.
     uint32 req = nvisited - furthestVisited;
     uint32 orig = uint32(furthestPath + 1) < npath ? furthestPath + 1 : npath;
-    uint32 size = npath - orig > 0 ? npath - orig : 0;
+    uint32 size = npath > orig ? npath - orig : 0;
     if (req + size > maxPath)
         size = maxPath - req;
 
